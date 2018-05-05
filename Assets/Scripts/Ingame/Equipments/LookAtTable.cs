73fe0553d62283074d7d949f3bf472b76ec7537a
YunihaPhyno <yunihaphyno@gmail.com>
2018-05-05 19:49:48 +0900

    ロックオンした敵を向き続けるUnit追加

---
+++ Assets/Scripts/Ingame/Equipments/LookAtTable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ingame
+{
+	public class LookAtTable : Equipment
+	{
+		private LockOnUnit m_lockOnUnit = new LockOnUnit();
+		public LockOnUnit GetLockOnUnit(){ return m_lockOnUnit; }
+
+		public override void InvokeFixedUpdate()
+		{
+			base.InvokeFixedUpdate();
+			UpdateRotation();
+		}
+
+		private void UpdateRotation()
+		{
+			Vector3 relativePos = GetLockOnUnit().GetTargetPosition() - transform.position;
+			transform.rotation = transform.rotation * Quaternion.FromToRotation(transform.rotation.eulerAngles, relativePos);
+		}
+	}
+}
---
+++ Assets/Scripts/Ingame/Unit/LockOnUnit.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ingame
+{
+	/// <summary>
+	/// 何かをロックオンしたいときに使う
+	/// </summary>
+	public class LockOnUnit
+	{
+		// ロックオンしたオブジェクトのTransformを格納する
+		private Transform m_targetTransform;
+
+		/// <summary>
+		/// ロックオン対象のターゲットをセット
+		/// </summary>
+		/// <param name="tgt">ロックオンするターゲットのTransfrom</param>
+		public void SetTarget(Transform tgt){ m_targetTransform = tgt; }
+
+		/// <summary>
+		/// ロックオンしたターゲット情報を破棄する
+		/// </summary>
+		public void DropTarget(){ m_targetTransform = null; }
+
+		/// <summary>
+		/// このユニットにターゲットは設定されている？
+		/// </summary>
+		/// <returns>ターゲットが設定されていれば<c>true</c></returns>
+		public bool TargetIsValid()
+		{
+			return (m_targetTransform != null);
+		}
+
+		/// <summary>
+		/// ロックオンしたターゲットの位置情報を取得する
+		/// </summary>
+		/// <returns>The target position.</returns>
+		public Vector3 GetTargetPosition()
+		{
+			if(m_targetTransform == null)
+			{
+				return null;
+			}
+			return m_targetTransform.position;
+		}
+	}
+}
